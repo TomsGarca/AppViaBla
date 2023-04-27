5a796a58689a68c3f15eb53cfb4e2c7cc40c3267
Tomas Garcia <toms_gr@hotmail.com>
2023-04-27 12:23:27 -0600

    Version: Acceso al mapa por Invitado

---
+++ MyAppViaBla/MyAppViaBla/app/src/main/java/com/ramkatom/myappviabla/Map.java
@@ -0,0 +1,74 @@
+package com.ramkatom.myappviabla;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.android.gms.maps.GoogleMap;
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentActivity;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class Map extends AppCompatActivity implements OnMapReadyCallback {
+
+    private MapView mapView;
+    private GoogleMap googleMap;
+
+    Button btn_host;
+    EditText editText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map);
+
+        mapView = findViewById(R.id.map_view);
+        mapView.onCreate(savedInstanceState);
+        mapView.getMapAsync(this);
+
+        editText = findViewById(R.id.textbox1);
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mapView.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mapView.onPause();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mapView.onDestroy();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mapView.onLowMemory();
+    }
+
+    @Override
+    public void onMapReady(GoogleMap map) {
+        googleMap = map;
+
+        LatLng BUAP_TUSA = new LatLng(19.00153, -98.20175);
+        googleMap.addMarker(new MarkerOptions().position(BUAP_TUSA).title("Marker in BUAP"));
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(BUAP_TUSA));
+    }
+}
--- MyAppViaBla/MyAppViaBla/app/src/main/java/com/ramkatom/myappviabla/Register.java
+++ MyAppViaBla/MyAppViaBla/app/src/main/java/com/ramkatom/myappviabla/Register.java
@@ -4,7 +4,6 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
-import android.graphics.drawable.GradientDrawable;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
@@ -19,15 +18,12 @@ import com.google.android.material.textfield.TextInputEditText;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
 
 public class Register extends AppCompatActivity {
     ///ELEMENTOS USADOS EN LA VISTA GRÁFICA
-    TextInputEditText editTextEmail, editTextPassword,editTextPassword2,editTextUser;
+    TextInputEditText editTextEmail, editTextPassword,editTextPassword2;
     Button btnRegister;
     FirebaseAuth mAuth;
-    FirebaseDatabase mDatabase = FirebaseDatabase.getInstance();
     ProgressBar progressBar;
     TextView textView;
     /// Cuando inicialices la actividad, verificar si el usuario ya accedió
@@ -35,7 +31,6 @@ public class Register extends AppCompatActivity {
     public void onStart() {
         super.onStart();
         FirebaseUser currentUser = mAuth.getCurrentUser();
-
         if(currentUser != null){
             Intent intent = new Intent(getApplicationContext(),MainActivity.class);
             startActivity(intent);
@@ -51,7 +46,6 @@ public class Register extends AppCompatActivity {
         editTextEmail = findViewById(R.id.email);
         editTextPassword = findViewById(R.id.password);
         editTextPassword2 = findViewById(R.id.password2);
-        editTextUser = findViewById(R.id.name);
         btnRegister = findViewById(R.id.btn_register);
         progressBar = findViewById(R.id.progressBar);
         textView = findViewById(R.id.loginNow);
@@ -66,24 +60,12 @@ public class Register extends AppCompatActivity {
                 finish();
             }
         });
-        /////REDONDEAR BOTON
-        GradientDrawable gradientDrawable = new GradientDrawable();
-        gradientDrawable.setColor(0xFF133343); // Cambia el color del botón
-        gradientDrawable.setCornerRadius(30); // Cambia el radio de los bordes
-        btnRegister.setBackground(gradientDrawable); // Establece el fondo del botón como el objeto GradientDrawable
         /// BOTÓN PARA REGISTRARSE EN LA APP
         btnRegister.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 progressBar.setVisibility(View.VISIBLE);
-                new Handler().postDelayed(new Runnable() {
-                    @Override
-                    public void run() {
-                        progressBar.setVisibility(View.GONE);
-                    }
-                }, 3000);
-                String email,password,password2,usuario;
-                usuario = String.valueOf(editTextUser.getText());
+                String email,password,password2;
                 email = String.valueOf(editTextEmail.getText());
                 password = String.valueOf(editTextPassword.getText());
                 password2 = String.valueOf(editTextPassword2.getText());
@@ -95,16 +77,20 @@ public class Register extends AppCompatActivity {
                     Toast.makeText(Register.this, "Ingresa una contraseña", Toast.LENGTH_SHORT).show();
                     return;
                 }
-                if(password == password2){
-                    Toast.makeText(Register.this, "La contraseña es diferente", Toast.LENGTH_SHORT).show();
+                if(password != password2){
+                    Toast.makeText(Register.this, "Contraseña no son iguales", Toast.LENGTH_SHORT).show();
                     return;
                 }
                 if(password.length()<8){
                     Toast.makeText(Register.this, "Contraseña debe ser mayor a 8 caracteres", Toast.LENGTH_SHORT).show();
                     return;
                 }
-
-
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        progressBar.setVisibility(View.GONE);
+                    }
+                }, 5000);
                 // FUNCIÓN QUE CONECTA CON FIRE BASE PARA LA AUTENTIFICACIÓN
                 mAuth.createUserWithEmailAndPassword(email, password)
                     .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
@@ -112,11 +98,6 @@ public class Register extends AppCompatActivity {
                         public void onComplete(@NonNull Task<AuthResult> task) {
                             progressBar.setVisibility(View.GONE);
                             if (task.isSuccessful()) {
-                                FirebaseUser user = mAuth.getCurrentUser();
-                                DatabaseReference mRef = mDatabase.getReference("usuarios").child(user.getUid());
-                                mRef.child("usuario").setValue(usuario);
-                                mRef.child("correo").setValue(email);
-
                                 Toast.makeText(Register.this, "Cuenta creada",
                                         Toast.LENGTH_SHORT).show();
                                 Intent intent = new Intent(getApplicationContext(),Login.class);
